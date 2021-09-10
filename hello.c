544444916f79247b87a3cb32fc68d3c5c203f287
madhurachobe <90455580+madhurachobe@users.noreply.github.com>
2021-09-10 17:29:56 +0530

    1 st date 10/09/2021 hello world

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ *  Display hellow world 
+ *
+ * @author (Madhura chobe)
+ * @version ( 10/09/2021 )
+ */
+public class samplehellow
+{
+    void main()
+    {
+        System.out.println("Hello World");
+    }
+    }
